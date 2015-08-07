18163c0890e6c1c4b0270a231e8abb8320bff82e
Ryan Stuczynski <ryan.stuczynski@gmail.com>
2015-08-07 16:10:12 +0000

    update fig again

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,4 +1,4 @@
 Instagram.configure do |config|
-  config.client_id = ENV["CONFIG_CLIENT_ID"]
-  config.client_secret = ENV["CONFIG_CLIENT_SECRET"]
+  config.client_id = ENV["CONFIG.CLIENT_ID"]
+  config.client_secret = ENV["CONFIG.CLIENT_SECRET"]
 end
